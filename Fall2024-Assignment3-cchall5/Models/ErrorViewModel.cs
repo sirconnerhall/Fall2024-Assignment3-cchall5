10d0e3650939018b38aafb5822f7b478ebbef265
Conner Hall <cchall5@crimson.ua.edu>
2024-10-16 12:09:10 -0500

    initial commit

---
+++ Fall2024-Assignment3-cchall5/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Fall2024_Assignment3_cchall5.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
